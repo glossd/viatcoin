4e881fb2b9b5b2d25f6afeb5984d42d74d202c00
Dennis Gloss <dennisgloss23@gmail.com>
2025-03-19 15:38:00 +0100

    longest chain rule

--- chain/sync.go
+++ chain/sync.go
@@ -7,20 +7,37 @@ import (
 	"math/big"
 )
 
-func Sync(apiUrl string) error {
-	err := downloadBlocks(apiUrl)
-	if err != nil {
-		return err
+func Sync(apiUrls []string) error {
+	// Longest Chain Rule
+	// Multiple valid chains may exist at the same time, but one eventually will outgrow another.
+	var longestChain []Block
+	var longestChainTotalWork = new(big.Int)
+	var longestChainUrl = ""
+	for _, url := range apiUrls {
+		// todo start working with the first chain
+		blocks, err := downloadBlocks(url)
+		if err != nil {
+			return err
+		}
+		newTotalWork := TotalWork(blocks)
+		if newTotalWork.Cmp(longestChainTotalWork) > 0 {
+			clear(longestChain) // help gc
+			longestChain = blocks
+			longestChainTotalWork = newTotalWork
+			longestChainUrl = url
+		} else {
+			clear(blocks) // help gc
+		}
 	}
-	err = downloadMempool(apiUrl)
+
+	setBlockchain(longestChain)
+
+	err := downloadMempool(longestChainUrl)
 	if err != nil {
 		return err
 	}
 	// todo synchronization must be constant
 
-	// todo Longest Chain Rule
-	// compare TotalWork of the blockchains
-	// Multiple valid chains may exist at the same time, but one eventually will outgrow another.
 	return nil
 }
 
@@ -32,21 +49,21 @@ func TotalWork(blocks []Block) *big.Int {
 	return acc
 }
 
-func downloadBlocks(apiUrl string) error {
+func downloadBlocks(apiUrl string) ([]Block, error) {
 	blocks, err := fetch.Get[[]Block](apiUrl + "/api/block?sort=asc&limit=-1")
 	if err != nil {
-		return err
+		return nil, err
 	}
 	// verifying the peer isn't corrupted
 	for i, block := range blocks {
 		if !block.Valid() {
-			return fmt.Errorf("invalid block found: index=%d, hash=%s", i, block.HashString())
+			return nil, fmt.Errorf("invalid block found: index=%d, hash=%s", i, block.HashString())
 		}
 
 		// verify chaining
 		if i > 0 {
 			if !bytes.Equal(block.PreviousHash, blocks[i-1].Hash()) {
-				return fmt.Errorf("corrupted blockchain: PreviousHash is wrong, index=%d", i)
+				return nil, fmt.Errorf("corrupted blockchain: PreviousHash is wrong, index=%d", i)
 			}
 		}
 
@@ -54,19 +71,23 @@ func downloadBlocks(apiUrl string) error {
 		for _, tx := range block.Transactions {
 			err := tx.Verify()
 			if err != nil {
-				return fmt.Errorf("invalid transaction found: %s, tx_id=%s, block_index=%d, block_hash=%s",
+				return nil, fmt.Errorf("invalid transaction found: %s, tx_id=%s, block_index=%d, block_hash=%s",
 					err, tx.ID, i, block.HashString())
 			}
 		}
 		if !bytes.Equal(block.MerkleRoot, calcMerkelRoot(block.Transactions)) {
-			return fmt.Errorf("invlaid block, markle root: index=%d, hash=%s", i, block.HashString())
+			return nil, fmt.Errorf("invlaid block, markle root: index=%d, hash=%s", i, block.HashString())
 		}
+	}
+	return blocks, nil
+}
 
+func setBlockchain(blocks []Block) {
+	for _, block := range blocks {
 		// whereas Bitcoin recontructs UTXO set, Viatcoin reconstruct the wallets.
 		MarkIngested(block.Transactions)
 		blockchain.Store(block.HashString(), block)
 	}
-	return nil
 }
 
 func downloadMempool(apiUrl string) error {
